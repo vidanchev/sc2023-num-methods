86cbf95a57dfd954c8f4adcbccde96a065afa301
viktor.d <viktor.d@endurosat.com>
2023-09-02 12:55:49 +0300

    Added Verlet method and checking its accuracy compared to Euler

--- Integrators.py
+++ Integrators.py
@@ -14,7 +14,7 @@ def rhs_oscillator( m , k , x , v ):
 
     return rhs
 
-# Integrate 1D harmonic oscillator
+# Integrate 1D harmonic oscillator with Euler method
 # Inputs:
 # - m: mass of the oscillator
 # - k: spring constant
@@ -56,5 +56,57 @@ def Euler_integrator( m , k , x0 , v0 , init_c ):
         x_sol[ i + 1 ] = x_sol[ i ] + v_sol[ i ]*dt
         v_sol[ i + 1 ] = v_sol[ i ] - k*x_sol[ i ]*dt/m
         '''
+    # Return the time array and solutions
+    return t_arr, x_sol, v_sol
+
+
+
+# Integrate 1D harmonic oscillator with Verlet method
+# Inputs:
+# - m: mass of the oscillator
+# - k: spring constant
+# - x0: initial position
+# - v0: initial velocity
+# - init_c: [ t_i , t_f , Nsteps (int) ]
+# Outputs:
+# - t_arr[ N ] -> time array
+# - x_sol[ N ] -> solution in position
+# - v_sol[ N ] -> solution in velocity
+def Verlet_integrator( m , k , x0 , v0 , init_c ):
+
+    # Unpackage the initial conditions vector into:
+    t_i = init_c[ 0 ] # Initial time
+    t_f = init_c[ 1 ] # Final time
+    Nsteps = int( init_c[ 2 ] ) # Number of steps -> INTEGER
+
+    # Create an array starting at t_i, ending at t_f with Nsteps steps
+    t_arr = np.linspace( t_i , t_f , Nsteps )
+    # Find our time step
+    dt = ( t_f - t_i )/( float( Nsteps ) - 1.0 )
+
+    # Initialize the arrays to hold the solution
+    x_sol = np.zeros( Nsteps )
+    v_sol = np.zeros( Nsteps )
+
+    # Pass the initial conditions to the solution
+    x_sol[ 0 ] = x0
+    v_sol[ 0 ] = v0
+
+    # INTEGRATE HERE - VERLET METHOD
+    for i in range( 0 , Nsteps - 1 ):
+        
+        # Get right-hand side in the initial moment "i"
+        rhs = rhs_oscillator( m , k , x_sol[ i ] , v_sol[ i ] )
+        # Compute the half-moment velocity (between "i" and "i+1")
+        v_half = v_sol[ i ] + rhs[ 1 ]*dt/2.0
+        # Use the half-moment velocity to compute RHS for position
+        rhs = rhs_oscillator( m , k , x_sol[ i ] , v_half )
+        # Compute the next step in position based on the half-velocity step
+        x_sol[ i + 1 ] = x_sol[ i ] + rhs[ 0 ]*dt
+        # Now we have position in "i+1" -> use it to get RHS for velocity to get there
+        rhs = rhs_oscillator( m , k , x_sol[ i + 1 ] , v_half )
+        # Compute next step velocity from positon in "i+1" and estimate in v_half
+        v_sol[ i + 1 ] = v_half + rhs[ 1 ]*dt/2.0
+
     # Return the time array and solutions
     return t_arr, x_sol, v_sol
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Integrators import Euler_integrator
+from Integrators import Euler_integrator, Verlet_integrator
 import matplotlib.pyplot as plt
 from numpy import pi
 import numpy as np
@@ -12,17 +12,19 @@ if __name__ == "__main__":
     # Initial conditions for the oscillator
     x0 = 1.0
     v0 = 0.0
-    init_c = [ 0.0 , 6.0*np.pi , 100 ] # [ t_i , t_f , Nsteps ]
+    init_c = [ 0.0 , 2.0*np.pi , 30 ] # [ t_i , t_f , Nsteps ]
     # Initial time, final time, number of steps in the vecotr
 
-    t_arr, x_arr, v_arr = Euler_integrator( m , k , x0 , v0 , init_c )
+    t_arr, x_E, v_E = Euler_integrator( m , k , x0 , v0 , init_c )
+    t_arr, x_V, v_V = Verlet_integrator( m , k , x0 , v0 , init_c )
 
     # Create the real solution for comparison
     omega = np.sqrt( k/m )
     x_real = np.cos( omega*t_arr )
 
     fig, ax = plt.subplots( )
-    ax.scatter( t_arr , x_arr , color = "red" , label = "Numerical" )
+    ax.scatter( t_arr , x_E , color = "red" , label = "Euler" )
+    ax.scatter( t_arr , x_V , color = "blue" , label = "Verlet" )
     ax.plot( t_arr , x_real , color = "green" , label = "Real" )
 
     ax.set_xlabel( "Time" )
